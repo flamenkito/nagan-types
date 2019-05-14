63f53c210d1001a62f08e68a29fe4bdc3a188339
Aleksey Sutov <asutov@outlook.com>
2019-05-14 12:40:07 +0300

    index

--- index.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/nagan';
-export * from './src/elements.document';
-export * from './src/layer.document';
-export * from './src/map.document';
-export * from './src/service.document';
-export * from './src/type.document';
-export * from './src/user.document';
