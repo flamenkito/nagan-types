9bebf0a64c156c5bc7a8f4b7ecc0bc4248b4ef65
Aleksey Sutov <asutov@outlook.com>
2019-05-22 11:09:26 +0300

    (feat) map type options

--- src/map.document.ts
+++ src/map.document.ts
@@ -13,15 +13,34 @@ export class MapDocument implements TypeDocument {
   @serializable(Serializers.value('map')) type = 'map';
 
   // MapDocument
-  @serializable(mapType) mapType: 'leaflet' | 'image';
   @serializable enabled: boolean;
   @serializable name: string;
   @serializable description: string;
   @serializable icon: string;
   @serializable(list(primitive())) availableLayerIds: string[];
+  @serializable(list(primitive())) visibleLayerIds: string[];
   @serializable(list(object(Nagan.Widget))) widgets: Nagan.Widget[];
+
+  @serializable(Serializers.mapOptions) options: MapOptions;
 
   static deserialize(data: Partial<MapDocument>): MapDocument {
     return deserialize(MapDocument, data);
   }
 }
+
+export namespace MapDocument {
+  export class LeafletOptions {
+    @serializable(Serializers.value('leaflet')) type = 'leaflet';
+    @serializable dpi: number;
+    @serializable(list(primitive())) center: [number, number];
+    @serializable zoom: number;
+  }
+
+  export class ImageOptions {
+    @serializable(Serializers.value('image')) type = 'image';
+    @serializable url: string;
+    @serializable(Serializers.anyType) style: any;
+  }
+}
+
+type MapOptions = MapDocument.LeafletOptions | MapDocument.ImageOptions;
--- src/serializers.ts
+++ src/serializers.ts
@@ -1,5 +1,7 @@
 import { custom, deserialize } from 'serializr';
+
 import { Nagan } from './nagan';
+import { MapDocument } from './map.document';
 
 export namespace Serializers {
   export const error = () => {
@@ -36,6 +38,17 @@ export namespace Serializers {
         return deserialize(Nagan.Widget.Fixed, value);
       default:
         throw new Error('Invalid position type');
+    }
+  });
+
+  export const mapOptions = custom(Serializers.error, value => {
+    switch (value.type) {
+      case 'image':
+        return deserialize(MapDocument.ImageOptions, value);
+      case 'leaflet':
+        return deserialize(MapDocument.LeafletOptions, value);
+      default:
+        throw new Error('Invalid map type');
     }
   });
 }
