1f14f1bd517213460898d7411c267ec1d6b031a0
Aleksey Sutov <asutov@outlook.com>
2019-06-11 16:00:22 +0300

    (ref) menus

--- src/menus.document.ts
+++ src/menus.document.ts
@@ -1,16 +1,36 @@
-import { serializable, deserialize, list, object } from 'serializr';
+import { serializable, deserialize, list, object, custom } from 'serializr';
 
 import { TypeDocument } from './type.document';
 import { Serializers } from './serializers';
 
 // models
+export class MapTarget {
+  @serializable(Serializers.value('map')) type = 'map';
+  @serializable mapId: string;
+}
+
+export class URLTarget {
+  @serializable(Serializers.value('url')) type = 'url';
+  @serializable url: string;
+}
+
+const asTarget = custom(Serializers.error, value => {
+  switch (value.type) {
+    case 'map':
+      return deserialize(MapTarget, value);
+    case 'url':
+      return deserialize(URLTarget, value);
+    default:
+      throw new Error('Invalid target type');
+  }
+});
+
 export class MenuRecord {
   @serializable id: number;
   @serializable title: string;
-  @serializable routerLink: string;
   @serializable href: string;
   @serializable icon: string;
-  @serializable target: string;
+  @serializable(asTarget) target: MapTarget | URLTarget;
   @serializable hasSubMenu: boolean;
   @serializable parentId: number;
 
